2310bec5f2fbe09e3df251c5e2bda18253d81840
Refki Santriono <refkisantriono@gmail.com>
2022-01-13 20:46:51 +0700

    tipe tipe data di java

---
+++ JAVA/JAVA_DASAR/src/TypeData.java
@@ -0,0 +1,101 @@
+public class TypeData {
+    public static void main(String[] args) {
+        // tipe data
+        // byte iniByte = 100;
+
+        // System.out.println(iniByte);
+
+        // konversi otomatis
+        // byte->short->int->long->float->double
+
+        // konversi manual
+        // double->float->long->int->short->byte
+
+        // string
+        // char r = 'R';
+        // char e = 'E';
+        // char f = 'F';
+        // System.out.print(r);
+        // System.out.print(e);
+        // System.out.print(f);
+
+        // String namaDepan = "Refki";
+        // String namaBelakang = "Santriono";
+        // String namaLengkap = namaDepan + " " +namaBelakang;
+
+        // System.out.println(namaDepan);
+        // System.out.println(namaBelakang);
+        // System.out.print(namaLengkap);;
+
+        // Variable
+
+        // String nama ;
+        // nama = "Refki Santriono";
+
+        // int umur = 30 ;
+
+        // var umur = 30;
+
+        // //variabel konstan tidak bisa diubah
+
+        // final String nama = "Refki Santriono";
+        // System.out.println("Nama : "+nama + " Umur :" +umur);
+
+        // int iniInt = 100 ;
+        // Integer iniObject = iniInt;
+
+        // short iniShort = iniObject.shortValue();
+        // System.out.println(iniShort);
+
+        // String [] myArray;
+        // myArray = new String[2];
+        // myArray[0] = "Refki";
+        // myArray[1] = "Santriono";
+
+        // System.out.print(myArray[0]);
+
+        // String[][] members = {
+        // {"Refki","Santriono"},
+        // {"Kiref","Triono"}
+        // };
+        // System.out.println(members[0][0]);
+        // System.out.println(members[0][1]);
+        // System.out.println(members[1][0]);
+        // System.out.println(members[1][1]);
+
+        // Operasi Matematika
+
+        // int a = 100;
+        // int b = 10;
+
+        // System.out.println(a + b);
+        // System.out.println(a - b);
+        // System.out.println(a * b);
+        // System.out.println(a / b);
+        // System.out.println(a % b);
+
+        // Augmanted Asign
+        int a = 100;
+
+        a += 9;
+        System.out.println(a);// 109
+
+        a -= 9;
+        System.out.println(a);// 100
+
+        a *= 9;
+        System.out.println(a);// 900
+
+        a /= 9;
+        System.out.println(a);// 100
+
+        a %= 9;
+        System.out.println(a);// 1
+
+        // Unary
+        a++;
+        System.out.println(a);// 2
+
+    }
+
+}
