092029651d6ef0b7509f869c9af5bd08e7772a63
Refki Santriono <refkisantriono@gmail.com>
2022-01-14 14:28:27 +0700

    constructor dengan parameter di class

---
+++ JAVA/JAVA_OOP/src/Motorcycle.java
@@ -0,0 +1,11 @@
+ class Motorcycle {
+    String name;
+    String brand;
+
+    Motorcycle(String paramName,String paramBrand){
+        name = paramName;
+        brand = paramBrand;
+    }
+
+
+}
---
+++ JAVA/JAVA_OOP/src/MotorcycleApp.java
@@ -0,0 +1,9 @@
+public class MotorcycleApp {
+
+    public static void main(String[] args) {
+        var motor1 = new Motorcycle("CBR","Honda");
+        System.out.println(motor1.name);
+        System.out.println(motor1.brand);
+
+    }
+}
