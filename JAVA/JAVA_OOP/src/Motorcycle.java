9957429ea865258171673c58bd686646cb32c88c
Refki Santriono <refkisantriono@gmail.com>
2022-01-14 14:37:28 +0700

    constructor overloading

--- JAVA/JAVA_OOP/src/Motorcycle.java
+++ JAVA/JAVA_OOP/src/Motorcycle.java
@@ -1,11 +1,17 @@
  class Motorcycle {
     String name;
     String brand;
+    String cc;
 
     Motorcycle(String paramName,String paramBrand){
         name = paramName;
         brand = paramBrand;
     }
+    Motorcycle(String paramCc){
+        cc = paramCc;
+    }
+    Motorcycle(){
 
+    }
 
 }
--- JAVA/JAVA_OOP/src/MotorcycleApp.java
+++ JAVA/JAVA_OOP/src/MotorcycleApp.java
@@ -2,8 +2,15 @@ public class MotorcycleApp {
 
     public static void main(String[] args) {
         var motor1 = new Motorcycle("CBR","Honda");
+        var motor2 = new Motorcycle("150");
+        var motor3 = new Motorcycle();
+            motor3.name = "King";
+            motor3.brand = "Yamaha";
+            motor3.cc = "125";
+
         System.out.println(motor1.name);
-        System.out.println(motor1.brand);
+        System.out.println(motor2.cc);
+        System.out.println(motor3.brand);
 
     }
 }
