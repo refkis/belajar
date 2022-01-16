fbefe9782df3792589f6aab93d66f4b064ec83a4
Refki Santriono <refkisantriono@gmail.com>
2022-01-16 11:39:32 +0700

    pewarisan pada java

---
+++ JAVA/JAVA_OOP/src/ManagerApp.java
@@ -0,0 +1,16 @@
+ class ManagerApp {
+    
+     
+     public static void main(String[] args) {
+         var manager1 = new Manager();
+         manager1.name = "Refki";
+         manager1.sayHello("Santri");
+
+
+         var vp1 =  new VicePresident();
+         vp1.name = "Kiref";
+         vp1.sayHello("Triono");
+
+        }
+}
+
